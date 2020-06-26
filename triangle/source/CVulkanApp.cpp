2e9f0b80b5fc956e12199ffc1be791b865647d0c
Martin Steen <martin.steen@ksti.de>
2020-06-26 16:11:58 +0200

    vertex buffers added

--- triangle/source/CVulkanApp.cpp
+++ triangle/source/CVulkanApp.cpp
@@ -1,6 +1,5 @@
 //***************************************************************************
 //
-//
 // @PROJECT  :    Vulkan Basis Klasse
 // @VERSION  :    1.0
 // @FILENAME :    CVulkanApp.cpp
@@ -9,6 +8,7 @@
 // @AUTHOR   :    Martin Steen
 // @EMAIL    :    martin.steen@ksti.de
 //
+// https://vulkan-tutorial.com
 //
 //***************************************************************************
 
@@ -109,19 +109,36 @@ void CVulkanApp::initWindow()
 
 void CVulkanApp::initVulkan()
 {
+    std::cout << "*** CVulkanApp::initVulkan START" << std::endl;
     createInstance();
+    std::cout << "  createInstance ok" << std::endl;
     setupDebugMessenger();
+    std::cout << "  setupDebugMessenger ok" << std::endl;
     createSurface();
+    std::cout << "  createSurface ok" << std::endl;
     pickPhysicalDevice();
+    std::cout << "  pickPhysicalDevice ok" << std::endl;
     createLogicalDevice();
+    std::cout << "  createLogicalDevice ok" << std::endl;
     createSwapChain();
+    std::cout << "  createSwapChain ok" << std::endl;
     createImageViews();
+    std::cout << "  createImageViews ok" << std::endl;
     createRenderPass();
+    std::cout << "  createRenderPass ok" << std::endl;
     createGraphicsPipeline();
+    std::cout << "  createGraphicsPipeline ok" << std::endl;
     createFramebuffers();
+    std::cout << "  createFramebuffers ok" << std::endl;
     createCommandPool();
+    std::cout << "  createCommandPool ok" << std::endl;
+    createVertexBuffer();
+    std::cout << "  createVertexBuffer ok" << std::endl;
     createCommandBuffers();
+    std::cout << "  createCommandBuffers ok" << std::endl;
     createSyncObjects();
+    std::cout << "  createSyncObjects ok" << std::endl;
+    std::cout << "*** initVulkan ok" << std::endl;
 }
 
 //---------------------------------------------------------------------------
@@ -144,25 +161,18 @@ void CVulkanApp::mainLoop()
 //---------------------------------------------------------------------------
 //
 // Klasse:    CVulkanApp
-// Methode:   cleanup
+// Methode:   cleanupSwapChain
 //
 //---------------------------------------------------------------------------
 
-void CVulkanApp::cleanup()
+void CVulkanApp::cleanupSwapChain()
 {
-    for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++)
-    {
-        vkDestroySemaphore(m_device, m_renderFinishedSemaphores[i], nullptr);
-        vkDestroySemaphore(m_device, m_imageAvailableSemaphores[i], nullptr);
-        vkDestroyFence(m_device, m_inFlightFences[i], nullptr);
-    }
-
-    vkDestroyCommandPool(m_device, m_commandPool, nullptr);
-
     for (auto framebuffer : m_swapChainFramebuffers)
     {
         vkDestroyFramebuffer(m_device, framebuffer, nullptr);
     }
+
+    vkFreeCommandBuffers(m_device, m_commandPool, static_cast<uint32_t>(m_commandBuffers.size()), m_commandBuffers.data());
 
     vkDestroyPipeline(m_device, m_graphicsPipeline, nullptr);
     vkDestroyPipelineLayout(m_device, m_pipelineLayout, nullptr);
@@ -174,20 +184,60 @@ void CVulkanApp::cleanup()
     }
 
     vkDestroySwapchainKHR(m_device, m_swapChain, nullptr);
+}
+
+//---------------------------------------------------------------------------
+//
+// Klasse:    CVulkanApp
+// Methode:   cleanup
+//
+//---------------------------------------------------------------------------
+
+void CVulkanApp::cleanup()
+{
+    std::cout << "*** CVulkanApp::cleanup START" << std::endl;
+    cleanupSwapChain();
+    std::cout << "  cleanupSwapChain OK" << std::endl;
+
+    vkDestroyBuffer(m_device, m_vertexBuffer, nullptr);
+    vkFreeMemory(m_device, m_vertexBufferMemory, nullptr);
+    std::cout << "  vkDestroyBuffer OK" << std::endl;
+
+    for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++)
+    {
+        vkDestroySemaphore(m_device, m_renderFinishedSemaphores[i], nullptr);
+        vkDestroySemaphore(m_device, m_imageAvailableSemaphores[i], nullptr);
+        vkDestroyFence(m_device, m_inFlightFences[i], nullptr);
+    }
+    std::cout << "  vkDestroySemaphore(s) OK" << std::endl;
+
+    vkDestroyCommandPool(m_device, m_commandPool, nullptr);
+    std::cout << "  vkDestroyCommandPool OK" << std::endl;
+
     vkDestroyDevice(m_device, nullptr);
+    std::cout << "  vkDestroyDevice OK" << std::endl;
 
     if (StaticEnableValidationLayers)
     {
         DestroyDebugUtilsMessengerEXT(m_instance, m_debugMessenger, nullptr);
     }
+    std::cout << "  DestroyDebugUtilsMessengerEXT OK" << std::endl;
 
     vkDestroySurfaceKHR(m_instance, m_surface, nullptr);
+    std::cout << "  vkDestroySurfaceKHR OK" << std::endl;
+
     vkDestroyInstance(m_instance, nullptr);
+    std::cout << "  vkDestroyInstance OK" << std::endl;
 
     glfwDestroyWindow(m_window);
+    std::cout << "  glfwDestroyWindow OK" << std::endl;
 
     glfwTerminate();
+    std::cout << "** CVulkanApp::cleanup OK" << std::endl;
 }
+
+
+
 
 //---------------------------------------------------------------------------
 //
@@ -536,6 +586,7 @@ void CVulkanApp::createRenderPass()
     }
 }
 
+#if 0
 //---------------------------------------------------------------------------
 //
 // Klasse:    CVulkanApp
@@ -657,6 +708,139 @@ void CVulkanApp::createGraphicsPipeline()
     vkDestroyShaderModule(m_device, fragShaderModule, nullptr);
     vkDestroyShaderModule(m_device, vertShaderModule, nullptr);
 }
+#endif
+
+//---------------------------------------------------------------------------
+//
+// Klasse:    CVulkanApp
+// Methode:   createGraphicsPipeline
+//
+//---------------------------------------------------------------------------
+
+void CVulkanApp::createGraphicsPipeline()
+{
+    auto vertShaderCode = readFile("shaders/shader-vert.spv");
+    auto fragShaderCode = readFile("shaders/shader-frag.spv");
+
+
+    VkShaderModule vertShaderModule = createShaderModule(vertShaderCode);
+    VkShaderModule fragShaderModule = createShaderModule(fragShaderCode);
+
+    VkPipelineShaderStageCreateInfo vertShaderStageInfo{};
+    vertShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    vertShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
+    vertShaderStageInfo.module = vertShaderModule;
+    vertShaderStageInfo.pName = "main";
+
+    VkPipelineShaderStageCreateInfo fragShaderStageInfo{};
+    fragShaderStageInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
+    fragShaderStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
+    fragShaderStageInfo.module = fragShaderModule;
+    fragShaderStageInfo.pName = "main";
+
+    VkPipelineShaderStageCreateInfo shaderStages[] = {vertShaderStageInfo, fragShaderStageInfo};
+
+    VkPipelineVertexInputStateCreateInfo vertexInputInfo{};
+    vertexInputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
+
+    auto bindingDescription = Vertex::getBindingDescription();
+    auto attributeDescriptions = Vertex::getAttributeDescriptions();
+
+    vertexInputInfo.vertexBindingDescriptionCount = 1;
+    vertexInputInfo.vertexAttributeDescriptionCount = static_cast<uint32_t>(attributeDescriptions.size());
+    vertexInputInfo.pVertexBindingDescriptions = &bindingDescription;
+    vertexInputInfo.pVertexAttributeDescriptions = attributeDescriptions.data();
+
+    VkPipelineInputAssemblyStateCreateInfo inputAssembly{};
+    inputAssembly.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
+    inputAssembly.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+    inputAssembly.primitiveRestartEnable = VK_FALSE;
+
+    VkViewport viewport{};
+    viewport.x = 0.0f;
+    viewport.y = 0.0f;
+    viewport.width = (float) m_swapChainExtent.width;
+    viewport.height = (float) m_swapChainExtent.height;
+    viewport.minDepth = 0.0f;
+    viewport.maxDepth = 1.0f;
+
+    VkRect2D scissor{};
+    scissor.offset = {0, 0};
+    scissor.extent = m_swapChainExtent;
+
+    VkPipelineViewportStateCreateInfo viewportState{};
+    viewportState.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
+    viewportState.viewportCount = 1;
+    viewportState.pViewports = &viewport;
+    viewportState.scissorCount = 1;
+    viewportState.pScissors = &scissor;
+
+    VkPipelineRasterizationStateCreateInfo rasterizer{};
+    rasterizer.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
+    rasterizer.depthClampEnable = VK_FALSE;
+    rasterizer.rasterizerDiscardEnable = VK_FALSE;
+    rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
+    rasterizer.lineWidth = 1.0f;
+    rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
+    rasterizer.frontFace = VK_FRONT_FACE_CLOCKWISE;
+    rasterizer.depthBiasEnable = VK_FALSE;
+
+    VkPipelineMultisampleStateCreateInfo multisampling{};
+    multisampling.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
+    multisampling.sampleShadingEnable = VK_FALSE;
+    multisampling.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
+
+    VkPipelineColorBlendAttachmentState colorBlendAttachment{};
+    colorBlendAttachment.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
+    colorBlendAttachment.blendEnable = VK_FALSE;
+
+    VkPipelineColorBlendStateCreateInfo colorBlending{};
+    colorBlending.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
+    colorBlending.logicOpEnable = VK_FALSE;
+    colorBlending.logicOp = VK_LOGIC_OP_COPY;
+    colorBlending.attachmentCount = 1;
+    colorBlending.pAttachments = &colorBlendAttachment;
+    colorBlending.blendConstants[0] = 0.0f;
+    colorBlending.blendConstants[1] = 0.0f;
+    colorBlending.blendConstants[2] = 0.0f;
+    colorBlending.blendConstants[3] = 0.0f;
+
+    VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
+    pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+    pipelineLayoutInfo.setLayoutCount = 0;
+    pipelineLayoutInfo.pushConstantRangeCount = 0;
+
+    if (vkCreatePipelineLayout(m_device, &pipelineLayoutInfo, nullptr, &m_pipelineLayout) != VK_SUCCESS)
+    {
+        throw std::runtime_error("failed to create pipeline layout!");
+    }
+
+    VkGraphicsPipelineCreateInfo pipelineInfo{};
+    pipelineInfo.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+    pipelineInfo.stageCount = 2;
+    pipelineInfo.pStages = shaderStages;
+    pipelineInfo.pVertexInputState = &vertexInputInfo;
+    pipelineInfo.pInputAssemblyState = &inputAssembly;
+    pipelineInfo.pViewportState = &viewportState;
+    pipelineInfo.pRasterizationState = &rasterizer;
+    pipelineInfo.pMultisampleState = &multisampling;
+    pipelineInfo.pColorBlendState = &colorBlending;
+    pipelineInfo.layout = m_pipelineLayout;
+    pipelineInfo.renderPass = m_renderPass;
+    pipelineInfo.subpass = 0;
+    pipelineInfo.basePipelineHandle = VK_NULL_HANDLE;
+
+    if (vkCreateGraphicsPipelines(m_device, VK_NULL_HANDLE, 1, &pipelineInfo, nullptr, &m_graphicsPipeline) != VK_SUCCESS)
+    {
+        throw std::runtime_error("failed to create graphics pipeline!");
+    }
+
+    vkDestroyShaderModule(m_device, fragShaderModule, nullptr);
+    vkDestroyShaderModule(m_device, vertShaderModule, nullptr);
+}
+
+
+
 
 //---------------------------------------------------------------------------
 //
@@ -695,6 +879,71 @@ void CVulkanApp::createFramebuffers()
 //---------------------------------------------------------------------------
 //
 // Klasse:    CVulkanApp
+// Methode:   findMemoryType
+//
+//---------------------------------------------------------------------------
+
+uint32_t CVulkanApp::findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties)
+{
+    VkPhysicalDeviceMemoryProperties memProperties;
+    vkGetPhysicalDeviceMemoryProperties(physicalDevice, &memProperties);
+
+    for (uint32_t i = 0; i < memProperties.memoryTypeCount; i++)
+    {
+        if ((typeFilter & (1 << i)) && (memProperties.memoryTypes[i].propertyFlags & properties) == properties)
+        {
+            return i;
+        }
+    }
+
+    throw std::runtime_error("failed to find suitable memory type!");
+}
+
+//---------------------------------------------------------------------------
+//
+// Klasse:    CVulkanApp
+// Methode:   createVertexBuffer
+//
+//---------------------------------------------------------------------------
+
+void CVulkanApp::createVertexBuffer()
+{
+    VkBufferCreateInfo bufferInfo{};
+    bufferInfo.sType = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO;
+    bufferInfo.size = sizeof(vertices[0]) * vertices.size();
+    bufferInfo.usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
+    bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+    if (vkCreateBuffer(m_device, &bufferInfo, nullptr, &m_vertexBuffer) != VK_SUCCESS)
+    {
+        throw std::runtime_error("failed to create vertex buffer!");
+    }
+
+    VkMemoryRequirements memRequirements;
+    vkGetBufferMemoryRequirements(m_device, m_vertexBuffer, &memRequirements);
+
+    VkMemoryAllocateInfo allocInfo{};
+    allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+    allocInfo.allocationSize = memRequirements.size;
+    allocInfo.memoryTypeIndex = findMemoryType(memRequirements.memoryTypeBits, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+
+    if (vkAllocateMemory(m_device, &allocInfo, nullptr, &m_vertexBufferMemory) != VK_SUCCESS)
+    {
+        throw std::runtime_error("failed to allocate vertex buffer memory!");
+    }
+
+    vkBindBufferMemory(m_device, m_vertexBuffer, m_vertexBufferMemory, 0);
+
+    void* data;
+    vkMapMemory(m_device, m_vertexBufferMemory, 0, bufferInfo.size, 0, &data);
+    memcpy(data, vertices.data(), (size_t) bufferInfo.size);
+    vkUnmapMemory(m_device, m_vertexBufferMemory);
+}
+
+
+//---------------------------------------------------------------------------
+//
+// Klasse:    CVulkanApp
 // Methode:   createCommandPool
 //
 //---------------------------------------------------------------------------
@@ -713,6 +962,7 @@ void CVulkanApp::createCommandPool()
     }
 }
 
+#if 0
 //---------------------------------------------------------------------------
 //
 // Klasse:    CVulkanApp
@@ -770,6 +1020,64 @@ void CVulkanApp::createCommandBuffers()
         }
     }
 }
+#endif
+
+void CVulkanApp::createCommandBuffers()
+{
+        m_commandBuffers.resize(m_swapChainFramebuffers.size());
+
+        VkCommandBufferAllocateInfo allocInfo{};
+        allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+        allocInfo.commandPool = m_commandPool;
+        allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+        allocInfo.commandBufferCount = (uint32_t) m_commandBuffers.size();
+
+        if (vkAllocateCommandBuffers(m_device, &allocInfo, m_commandBuffers.data()) != VK_SUCCESS)
+        {
+            throw std::runtime_error("failed to allocate command buffers!");
+        }
+
+        for (size_t i = 0; i < m_commandBuffers.size(); i++)
+        {
+            VkCommandBufferBeginInfo beginInfo{};
+            beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+
+            if (vkBeginCommandBuffer(m_commandBuffers[i], &beginInfo) != VK_SUCCESS)
+            {
+                throw std::runtime_error("failed to begin recording command buffer!");
+            }
+
+            VkRenderPassBeginInfo renderPassInfo{};
+            renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+            renderPassInfo.renderPass = m_renderPass;
+            renderPassInfo.framebuffer = m_swapChainFramebuffers[i];
+            renderPassInfo.renderArea.offset = {0, 0};
+            renderPassInfo.renderArea.extent = m_swapChainExtent;
+
+            VkClearValue clearColor = {0.0f, 0.0f, 0.0f, 1.0f};
+            renderPassInfo.clearValueCount = 1;
+            renderPassInfo.pClearValues = &clearColor;
+
+            vkCmdBeginRenderPass(m_commandBuffers[i], &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
+
+            vkCmdBindPipeline(m_commandBuffers[i], VK_PIPELINE_BIND_POINT_GRAPHICS, m_graphicsPipeline);
+
+            VkBuffer vertexBuffers[] = { m_vertexBuffer };
+            VkDeviceSize offsets[] = {0};
+            vkCmdBindVertexBuffers(m_commandBuffers[i], 0, 1, vertexBuffers, offsets);
+
+            vkCmdDraw(m_commandBuffers[i], static_cast<uint32_t>(vertices.size()), 1, 0, 0);
+
+            vkCmdEndRenderPass(m_commandBuffers[i]);
+
+            if (vkEndCommandBuffer(m_commandBuffers[i]) != VK_SUCCESS)
+            {
+                throw std::runtime_error("failed to record command buffer!");
+            }
+        }
+    }
+
+
 
 //---------------------------------------------------------------------------
 //
--- triangle/source/CVulkanApp.h
+++ triangle/source/CVulkanApp.h
@@ -1,5 +1,6 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 
 #include <iostream>
 #include <fstream>
@@ -36,6 +37,45 @@ struct SwapChainSupportDetails
     std::vector<VkPresentModeKHR> presentModes;
 };
 #endif
+
+
+struct Vertex {
+    glm::vec2 pos;
+    glm::vec3 color;
+
+    static VkVertexInputBindingDescription getBindingDescription() {
+        VkVertexInputBindingDescription bindingDescription{};
+        bindingDescription.binding = 0;
+        bindingDescription.stride = sizeof(Vertex);
+        bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+
+        return bindingDescription;
+    }
+
+    static std::array<VkVertexInputAttributeDescription, 2> getAttributeDescriptions() {
+        std::array<VkVertexInputAttributeDescription, 2> attributeDescriptions{};
+
+        attributeDescriptions[0].binding = 0;
+        attributeDescriptions[0].location = 0;
+        attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
+        attributeDescriptions[0].offset = offsetof(Vertex, pos);
+
+        attributeDescriptions[1].binding = 0;
+        attributeDescriptions[1].location = 1;
+        attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+        attributeDescriptions[1].offset = offsetof(Vertex, color);
+
+        return attributeDescriptions;
+    }
+};
+
+const std::vector<Vertex> vertices = {
+    {{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+    {{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
+    {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}
+};
+
+
 
 class CVulkanApp
 {
@@ -81,6 +121,10 @@ class CVulkanApp
         VkCommandPool m_commandPool;
         std::vector<VkCommandBuffer> m_commandBuffers;
 
+        VkBuffer m_vertexBuffer;
+        VkDeviceMemory m_vertexBufferMemory;
+
+
         std::vector<VkSemaphore> m_imageAvailableSemaphores;
         std::vector<VkSemaphore> m_renderFinishedSemaphores;
         std::vector<VkFence> m_inFlightFences;
@@ -89,6 +133,7 @@ class CVulkanApp
 
         void initWindow();
         void initVulkan();
+        void cleanupSwapChain();
         void cleanup();
         void createInstance();
         void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
@@ -102,8 +147,10 @@ class CVulkanApp
         void createGraphicsPipeline();
         void createFramebuffers();
         void createCommandPool();
+        void createVertexBuffer();
         void createCommandBuffers();
         void createSyncObjects();
+        uint32_t findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties);
         void mainLoop();
 
         VkShaderModule createShaderModule(const std::vector<char>& code) const;
